6fcffb5c78683a14943d101e966a1b2e886ee939
zhuhongchang <featherwit96@163.com>
2021-04-14 22:56:22 +0800

    作业讲解 参数化讲解部分（yaml文件实现参数化,debugtalk.py简单实现参数化

--- debugtalk.py
+++ debugtalk.py
@@ -16,5 +16,11 @@ def teardown_test_case(content):
 def get_condition():
     return []
 
+def get_params_01():
+    return ["压","newdream","12306"]
+
+def get_params_02():
+    return [["newdream","newdream_百度搜索"],["12306","12306_百度搜索"]]
+
 if __name__ == "__main__":
     print(get_value())
